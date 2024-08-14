b7bbfbf90038fdf32d713af87279d154fe0220fe
rush <rush@wusaby.com>
2024-08-14 14:56:46 +0300

    add typography comments

--- src/components/MuiTypography.tsx
+++ src/components/MuiTypography.tsx
@@ -1,11 +1,15 @@
 import { Typography } from '@mui/material'
 
+// 1. 
+
 export const MuiTypography = () => {
   return (
     <div>
       <Typography variant='h1'>h1 Heading</Typography>
       <Typography variant='h2'>h2 Heading</Typography>
       <Typography variant='h3'>h3 Heading</Typography>
+      {/* change semantic html with component */}
+      {/* add bottom margin with gutterBottom */}
       <Typography variant='h4' component='h1' gutterBottom>
         h4 Heading
       </Typography>
@@ -13,6 +17,7 @@ export const MuiTypography = () => {
       <Typography variant='h6'>h6 Heading</Typography>
       <Typography variant='subtitle1'>subtitle1</Typography>
       <Typography variant='subtitle2'>subtitle2</Typography>
+      {/* default is variant='body1' */}
       <Typography>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam saepe
         magnam illo quas illum minus, aperiam, iusto optio quisquam veniam
